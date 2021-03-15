5d70cc7c7082bb63029613d94b2fefc6f3ab6c81
MatteoTHP <wow-guerrier@hotmail.fr>
2021-03-15 17:50:10 +0100

    deleted user_mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'no-reply@monsite.fr'
- 
-  def welcome_email
-    
-    mail(to: "avion@yopmail.com", subject: 'Bienvenue chez nous !') 
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :welcome_send
-
   validates :rank, presence: true
 
   has_many :notations
@@ -10,7 +8,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  def welcome_send
-    UserMailer.welcome_email.deliver_now
-  end
 end
