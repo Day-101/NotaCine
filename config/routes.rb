4819b868d6ce5bb9e36c8355a230b9053142aae3
ZugastiKevin <rochambeaut@gmail.com>
2021-03-16 20:50:22 +0100

    add road

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :movie
 
-  get "/team", to: "static#team"
-  get "/contact", to: "static#contact"
-  get "/rgpd", to: "static#rgpd"
+  get "/team", to: "statics#team"
+  get "/contact", to: "statics#contact"
+  get "/rgpd", to: "statics#rgpd"
 
-  
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
