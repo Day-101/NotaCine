bbdc2cf468bd9dff61e0709fca8fc104414f6023
MatteoTHP <wow-guerrier@hotmail.fr>
2021-03-23 12:43:03 +0100

    fixed alerts

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -9,7 +9,7 @@ before_action :redirect_if_not_admin
 
   def create
     user = User.find_by(email: admin_params[:user_email])
-    user.rank = admin_params[:rank].to_i
+    user.rank = admin_params[:rank].to_i 
     if user.save
       flash.notice = "User : #{user.email} (#{user.username}) is now rank : #{user.rank}"
     else
