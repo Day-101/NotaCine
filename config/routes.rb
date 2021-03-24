62ba5ffb1bd3a788a40674dc5ae6a1ca1a1a22c5
Neka <nekoanchor@yahoo.fr>
2021-03-24 19:44:55 +0100

    re-add routes

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :movies do
+    resources :notations, only: [:new, :create, :edit, :update]
+    resources :comments, except: [:new, :show, :index]
+  end
+  resources :users, only: [:show] do
+  	resources :avatars, only: [:create]
+  end
+  root to: 'movies#index'
+  get 'statics/team'
+  get 'statics/contact'
+  get 'statics/privacy'
+  get 'statics/how'
+
+  get "/search", to: "searches#new"
+  resources :searches, only: [:create]
+
+  get "/admin", to: "admins#show"
+  resources :admins #, only: [:update, :edit]
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
