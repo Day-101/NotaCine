c54b135ad34f0ed691c78060b06480cd968aab57
MatteoTHP <wow-guerrier@hotmail.fr>
2021-03-24 15:05:29 +0100

    Cleaner models calls in admin dashboard

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,7 +4,10 @@ before_action :redirect_if_not_admin
 
 
   def show
-
+    @extrems_notations = Notation.extreme_notations
+    @articles = Article.all
+    @movies = Movie.all
+    @users = User.all
   end
 
 end
